75e691ba40cfb11220d91a41f7dd0c92b3f0acb9
MyungSeok, Seo <ms29.seo@gmail.com>
2016-05-14 16:56:39 +0900

    fix bug

--- src/net/brainage/sample/LottoGenerator.java
+++ src/net/brainage/sample/LottoGenerator.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- *
  * @author <a href="mailto:ms29.seo@gmail.com">ms29.seo</a>
  */
 public class LottoGenerator {
@@ -17,11 +16,12 @@ public class LottoGenerator {
 
     public int[] generate() {
         int[] result = new int[6];
-        for(int i = 0; i < 6; i++) {
-            result[i] = random.nextInt(45);
+        for (int i = 0; i < 6; i++) {
+            result[i] = random.nextInt(45) + 1;
         }
         return result;
     }
+
     public static void main(String[] args) {
         int[] result = new LottoGenerator().generate();
         System.out.println(Arrays.toString(result));
