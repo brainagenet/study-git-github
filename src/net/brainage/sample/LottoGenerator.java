e4e08598ca28aaa17504b026d8f35df1c8cc583c
MyungSeok, Seo <ms29.seo@gmail.com>
2016-05-16 01:11:33 +0900

    간결하게 로직 변경

--- src/net/brainage/sample/LottoGenerator.java
+++ src/net/brainage/sample/LottoGenerator.java
@@ -5,7 +5,9 @@
 package net.brainage.sample;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:ms29.seo@gmail.com">ms29.seo</a>
@@ -14,16 +16,21 @@ public class LottoGenerator {
 
     private Random random = new Random(System.nanoTime());
 
-    public int[] generate() {
-        int[] result = new int[6];
-        for (int i = 0; i < 6; i++) {
-            result[i] = random.nextInt(45) + 1;
+    public Integer[] generate() {
+        Set<Integer> generated = new HashSet<>();
+        while (generated.size() < 6) {
+            int num = random.nextInt(45) + 1;
+            if (!generated.contains(num)) {
+                generated.add(num);
+            }
         }
-        return result;
+        Integer[] results = generated.toArray(new Integer[6]);
+        return results;
     }
 
     public static void main(String[] args) {
-        int[] result = new LottoGenerator().generate();
+        Integer[] result = new LottoGenerator().generate();
         System.out.println(Arrays.toString(result));
     }
+
 }
